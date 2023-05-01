a29c40302c07885bc4fb236afbaded603c873497
Iggos <jonskiigor@gmail.com>
2023-05-01 15:46:39 +0200

    Add login screen with logo and email text field

---
+++ app/src/main/java/com/app/limboapp/ui/LoginScreen.kt
@@ -0,0 +1,143 @@
+package com.app.limboapp.ui
+
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.app.limboapp.R
+import com.app.limboapp.ui.theme.*
+
+@Preview
+@Composable
+fun LoginScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(ButtonBlack)
+    ) {
+        Column {
+            Spacer(modifier = Modifier.height(20.dp))
+            LimboLogo()
+            LoginTextFields()
+        }
+    }
+}
+
+@Composable
+fun LimboLogo() {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.limbo_flame),
+            contentDescription = "Limbo Logo",
+            modifier = Modifier
+                .size(48.dp)
+        )
+        Text(
+            text = "Limbo",
+            fontSize = 36.sp,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.SemiBold,
+            color = TextOrange
+        )
+    }
+}
+
+@Composable
+fun LoginTextFields() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Logowanie",
+            color = TextWhite,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 32.sp
+        )
+        Spacer(modifier = Modifier.height(26.dp))
+
+        var email by rememberSaveable {
+            mutableStateOf("")
+        }
+        TextField(
+            modifier = Modifier
+                .clip(RoundedCornerShape(20.dp))
+                .width(300.dp),
+            value = email,
+            onValueChange = {
+                email = it
+            },
+            shape = RoundedCornerShape(20.dp),
+            placeholder = {
+                Text(
+                    text = "Email użytkownika",
+                    color = TextWhite,
+                    fontFamily = Montserrat,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 15.sp
+                )
+            },
+            textStyle = TextStyle(
+                color = TextWhite
+            ),
+            trailingIcon = {
+                IconButton(onClick = {
+
+                }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.user_gradient_icon),
+                        contentDescription = "Email użytkownika",
+                        modifier = Modifier.padding(12.dp)
+                    )
+                }
+            },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Default
+            ),
+            keyboardActions = KeyboardActions(
+                onAny = {
+                    Log.d("LOG_TAG", "Clicked email done")
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = TextWhite,
+                disabledTextColor = TextWhite,
+                backgroundColor = DarkGray,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent
+            )
+        )
+    }
+    
+}
--- app/src/main/java/com/app/limboapp/ui/theme/Color.kt
+++ app/src/main/java/com/app/limboapp/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.app.limboapp.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
@@ -13,3 +14,13 @@ val DarkOrange = Color(0xFFF92100)
 val MediumOrange = Color(0xFFFA5F00)
 val LightOrange = Color(0xFFFCa401)
 val ButtonBlack = Color(0xFF0D0D0D)
+val TextOrange = Color(0xFFEC5500)
+val DarkGray = Color(0xFF252525)
+
+val horizontalOrangeGradient = Brush.horizontalGradient(
+    colors = listOf(
+        DarkOrange,
+        MediumOrange,
+        LightOrange
+    )
+)
