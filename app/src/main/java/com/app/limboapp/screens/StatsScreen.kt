d426b994e436835e82845bafe122928ab978bb40
Iggos <jonskiigor@gmail.com>
2023-05-29 22:31:05 +0200

    Add gained bonus row

--- app/src/main/java/com/app/limboapp/screens/StatsScreen.kt
+++ app/src/main/java/com/app/limboapp/screens/StatsScreen.kt
@@ -6,14 +6,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -127,6 +134,39 @@ fun GainedBonusCard(
         )
     }
 }
+
+@Composable
+fun GainedBonusRow() {
+
+    val localDensity = LocalDensity.current
+
+    var tallestColumnHeightDp by remember { mutableStateOf(10.dp) }
+
+    // TODO ask if it isn't a  infinite recomposition (could I use height on Row?)
+
+    Row(
+        modifier = Modifier
+            .onGloballyPositioned { coordinates ->
+                tallestColumnHeightDp = with(localDensity) {
+                    coordinates.size.height.toDp()
+                }
+            },
+        horizontalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        GainedBonusCard(
+            modifier = Modifier
+                .weight(1f),
+            text = "Dodatkowe punkty na egzaminie"
+        )
+        GainedBonusCard(
+            modifier = Modifier
+                .weight(1f)
+                .height(tallestColumnHeightDp),
+            text = "Short xD"
+        )
+    }
+}
+
 //-----------------------------------------
 
 @Preview
@@ -148,4 +188,10 @@ fun StatsSectionPreview() {
 @Composable
 fun GainedBonusCardPreview() {
     GainedBonusCard(text = "Dodatkowe punkty na egzaminie")
+}
+
+@Preview(widthDp = 300)
+@Composable
+fun GainedBonusRowPreview() {
+    GainedBonusRow()
 }
