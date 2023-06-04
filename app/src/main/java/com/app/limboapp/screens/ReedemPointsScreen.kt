1880340162d1542ceb6edb47ffbf2c1e62ccafaa
Iggos <jonskiigor@gmail.com>
2023-06-04 19:00:25 +0200

    LI-1 | Create redeem points screen file with empty composables

---
+++ app/src/main/java/com/app/limboapp/screens/ReedemPointsScreen.kt
@@ -0,0 +1,17 @@
+package com.app.limboapp.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RedeemPointsScreen() {
+
+}
+
+// --------------------
+
+@Preview
+@Composable
+fun RedeemPointsScreenPreview() {
+    RedeemPointsScreen()
+}
