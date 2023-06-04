31eb2c66cce1863435ea2f7cb092cb8a5230c709
Iggos <jonskiigor@gmail.com>
2023-06-04 18:35:07 +0200

    Add stats screen layout

--- app/src/main/java/com/app/limboapp/screens/StatsScreen.kt
+++ app/src/main/java/com/app/limboapp/screens/StatsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -27,6 +28,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.app.limboapp.nav.LimboBottomNavigation
 import com.app.limboapp.ui.theme.BlackBackground
 import com.app.limboapp.ui.theme.Montserrat
 import com.app.limboapp.ui.theme.TextWhite
@@ -35,7 +37,17 @@ import com.app.limboapp.ui.theme.orangeGradient
 
 @Composable
 fun StatsScreen() {
-
+    Scaffold(
+        backgroundColor = BlackBackground,
+        topBar = {
+            LimboLogoWithPointsAndProfile()
+        },
+        bottomBar = {
+            LimboBottomNavigation()
+        }
+    ) { paddingValues ->
+        StatsScreenContent(modifier = Modifier.padding(paddingValues))
+    }
 }
 
 @Composable
@@ -243,4 +255,10 @@ fun GainedBonusSectionPreview() {
 @Composable
 fun StatsScreenContentPreview() {
     StatsScreenContent()
+}
+
+@Preview
+@Composable
+fun StatsScreenPreview() {
+    StatsScreen()
 }
