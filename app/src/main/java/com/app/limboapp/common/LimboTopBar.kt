a8e67421f057f759a56fc31a7cbff9ebbace40d9
Iggos <jonskiigor@gmail.com>
2023-06-05 11:24:43 +0200

    LI-8 | Create common package with top bar composable file

---
+++ app/src/main/java/com/app/limboapp/common/LimboTopBar.kt
@@ -0,0 +1,17 @@
+package com.app.limboapp.common
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun LimboTopBar() {
+
+}
+
+// --------------------
+
+@Preview
+@Composable
+fun LimboTopBarPreview() {
+    LimboTopBar()
+}
