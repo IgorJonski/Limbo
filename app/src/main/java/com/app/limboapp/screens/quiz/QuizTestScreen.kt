8c152f4c2cb91e1a77e6463e38d2aa1da0b76e0e
Iggos <jonskiigor@gmail.com>
2023-07-02 12:16:36 +0200

    LI-11 | Add quiz data class and start of quiz question composable

--- app/src/main/java/com/app/limboapp/screens/quiz/QuizTestScreen.kt
+++ app/src/main/java/com/app/limboapp/screens/quiz/QuizTestScreen.kt
@@ -1,10 +1,13 @@
 package com.app.limboapp.screens.quiz
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,12 +19,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.limboapp.R
+import com.app.limboapp.model.Question
 import com.app.limboapp.ui.theme.BackgroundWhite
 import com.app.limboapp.ui.theme.Montserrat
 import com.app.limboapp.ui.theme.TextWhite
@@ -102,6 +108,36 @@ fun QuizProgressSection(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun QuizQuestion(
+    modifier: Modifier = Modifier,
+    question: Question
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = question.text,
+            color = TextWhite,
+            fontFamily = Montserrat,
+            fontWeight = FontWeight.Medium,
+            fontSize = 17.sp,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.height(18.dp))
+        if (question.image != null) {
+            Image(
+                painter = painterResource(id = question.image),
+                contentDescription = "Question image",
+                modifier = Modifier
+                    .fillMaxWidth(0.7f),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
 // --------------------
 
 @Preview
@@ -126,4 +162,16 @@ fun QuizQuestionsLeftPreview() {
 @Composable
 fun QuizProgressSectionPreview() {
     QuizProgressSection()
+}
+
+@Preview
+@Composable
+fun QuizQuestionPreview() {
+    val testQuestion = Question(
+        text = "Ile będzie równa zmienna licznik po wykonaniu tego kodu?",
+        image = R.drawable.quiz1,
+        wrongAnswers = arrayListOf("5", "0", "3"),
+        correctAnswer = "-5"
+    )
+    QuizQuestion(question = testQuestion)
 }
